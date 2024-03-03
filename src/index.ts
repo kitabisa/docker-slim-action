341ada0a644bfda98bea02b1534a06027128d7e7
muhfaris <devmuhfaris@gmail.com>
2024-03-03 07:28:28 +0700

    fix: fixing download docker slim

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,43 @@
-import {
-  core, fs, https, io, os,
-  path, shell, TMP_DIR
-} from './const';
+import { core, fs, https, io, os, path, shell, TMP_DIR } from "./const";
 
-const inputOverwrite = core.getBooleanInput('overwrite', {required: false});
-const inputTarget = core.getInput('target', {required: true});
-const inputVersion = core.getInput('version', {required: false});
-let inputTag = core.getInput('tag', {required: true});
+const inputOverwrite = core.getBooleanInput("overwrite", { required: false });
+const inputTarget = core.getInput("target", { required: true });
+const inputVersion = core.getInput("version", { required: false });
+let inputTag = core.getInput("tag", { required: true });
 
-let SLIM_PATH = '';
+let SLIM_PATH = "";
 
 async function get_slim() {
-  let DIST = '';
-  let EXT = '';
-  let FILENAME = '';
-  let KERNEL = '';
-  let MACHINE = '';
-  let URL = '';
-  let VER = '';
+  let DIST = "";
+  let EXT = "";
+  let FILENAME = "";
+  let KERNEL = "";
+  let MACHINE = "";
+  let URL = "";
+  let VER = "";
 
   // Get the current released tag_name
   const options = {
-    hostname: 'api.github.com',
-    path: '/repos/slimtoolkit/slim/releases',
-    headers: { 'User-Agent': 'Mozilla/5.0' },
+    hostname: "api.github.com",
+    path: "/repos/slimtoolkit/slim/releases",
+    headers: { "User-Agent": "Mozilla/5.0" },
   };
   const response = await new Promise((resolve, reject) => {
-    https.get(options, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
+    https
+      .get(options, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", (error) => {
+        reject(error);
       });
-      res.on('end', () => {
-        resolve(JSON.parse(data));
-      });
-    }).on('error', (error) => {
-      reject(error);
-    });
   });
-  
+
   try {
     if (inputVersion == "" || inputVersion == "latest") {
       VER = response[0].tag_name;
@@ -46,31 +45,33 @@ async function get_slim() {
       VER = inputVersion;
     }
   } catch {
-    throw new Error('ERROR! Could not retrieve the current Slim version number.');
+    throw new Error(
+      "ERROR! Could not retrieve the current Slim version number.",
+    );
   }
-  
-  URL = `https://downloads.dockerslim.com/releases/${VER}`;
+
+  URL = `https://github.com/slimtoolkit/slim/releases/${VER}`;
 
   // Get kernel name and machine architecture.
   KERNEL = os.platform();
   MACHINE = os.arch();
 
   // Determine the target distrubution
-  if (KERNEL === 'linux') {
-    EXT = 'tar.gz';
-    if (MACHINE === 'x64') {
-      DIST = 'linux';
-    } else if (MACHINE === 'arm') {
-      DIST = 'linux_arm';
-    } else if (MACHINE === 'arm64') {
-      DIST = 'linux_arm64';
+  if (KERNEL === "linux") {
+    EXT = "tar.gz";
+    if (MACHINE === "x64") {
+      DIST = "linux";
+    } else if (MACHINE === "arm") {
+      DIST = "linux_arm";
+    } else if (MACHINE === "arm64") {
+      DIST = "linux_arm64";
     }
-  } else if (KERNEL === 'darwin') {
-    EXT = 'zip';
-    if (MACHINE === 'x64') {
-      DIST = 'mac';
-    } else if (MACHINE === 'arm64') {
-      DIST = 'mac_m1';
+  } else if (KERNEL === "darwin") {
+    EXT = "zip";
+    if (MACHINE === "x64") {
+      DIST = "mac";
+    } else if (MACHINE === "arm64") {
+      DIST = "mac_m1";
     }
   } else {
     throw new Error(`ERROR! ${KERNEL} is not a supported platform.`);
@@ -86,32 +87,34 @@ async function get_slim() {
 
   const file = fs.createWriteStream(path.join(TMP_DIR, FILENAME));
   await new Promise((resolve, reject) => {
-    https.get(`${URL}/${FILENAME}`, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
-        resolve(file);
+    https
+      .get(`${URL}/${FILENAME}`, (response) => {
+        response.pipe(file);
+        file.on("finish", () => {
+          file.close();
+          resolve(file);
+        });
+      })
+      .on("error", (error) => {
+        fs.unlinkSync(path.join(TMP_DIR, FILENAME));
+        reject(error);
       });
-    }).on('error', (error) => {
-      fs.unlinkSync(path.join(TMP_DIR, FILENAME));
-      reject(error);
-    });
   });
 
   core.debug(`Unpacking ${path.join(TMP_DIR, FILENAME)}`);
-  if (EXT === 'zip') {
-    const extract = require('extract-zip');
+  if (EXT === "zip") {
+    const extract = require("extract-zip");
     await extract(path.join(TMP_DIR, FILENAME), {
-      dir: TMP_DIR
+      dir: TMP_DIR,
     });
-  } else if (EXT === 'tar.gz') {
-    const tar = require('tar');
+  } else if (EXT === "tar.gz") {
+    const tar = require("tar");
     await tar.x({
       file: path.join(TMP_DIR, FILENAME),
-      cwd: TMP_DIR
+      cwd: TMP_DIR,
     });
   } else {
-    throw new Error('ERROR! Unexpected file extension.');
+    throw new Error("ERROR! Unexpected file extension.");
   }
 
   SLIM_PATH = path.join(TMP_DIR, `dist_${DIST}`);
@@ -121,47 +124,54 @@ async function get_slim() {
 }
 
 async function run() {
-  core.debug('Downloading slim');
+  core.debug("Downloading slim");
   await get_slim();
 
   core.info(`slim on target: ${inputTarget}`);
 
-  let args = ['b', '--target', inputTarget];
+  let args = ["b", "--target", inputTarget];
 
-  if (process.env['DSLIM_HTTP_PROBE_OFF'] == 'true' || process.env['DSLIM_HTTP_PROBE'] == 'false') {
-    if (typeof process.env['DSLIM_CONTINUE_AFTER'] === 'undefined') args.push('--continue-after', '1')
+  if (
+    process.env["DSLIM_HTTP_PROBE_OFF"] == "true" ||
+    process.env["DSLIM_HTTP_PROBE"] == "false"
+  ) {
+    if (typeof process.env["DSLIM_CONTINUE_AFTER"] === "undefined")
+      args.push("--continue-after", "1");
   }
 
-  await shell.exec('slim', args, {cwd: SLIM_PATH});
+  await shell.exec("slim", args, { cwd: SLIM_PATH });
 
-  const data = fs.readFileSync(path.join(SLIM_PATH, 'slim.report.json'));
+  const data = fs.readFileSync(path.join(SLIM_PATH, "slim.report.json"));
   const report = JSON.parse(data);
 
-  core.setOutput('report', report);
+  core.setOutput("report", report);
 
-  if (report.state == 'error') {
-    throw new Error('ERROR! Cannot build over target');
+  if (report.state == "error") {
+    throw new Error("ERROR! Cannot build over target");
   }
 
-  const [image, tag] = report.target_reference.split(':')
+  const [image, tag] = report.target_reference.split(":");
 
   if (inputOverwrite && tag) {
     core.info(`Overwriting ${image}:${tag} with slimmed version`);
-    inputTag = tag
+    inputTag = tag;
 
-    await shell.exec('docker image', ['rm', report.target_reference]);
+    await shell.exec("docker image", ["rm", report.target_reference]);
   }
 
-  await shell.exec('docker tag', [report.minified_image, `${image}:${inputTag}`]);
-  await shell.exec('docker image', ['rm', report.minified_image]);
+  await shell.exec("docker tag", [
+    report.minified_image,
+    `${image}:${inputTag}`,
+  ]);
+  await shell.exec("docker image", ["rm", report.minified_image]);
 }
 
 if (inputTag == "") {
-  core.setFailed('ERROR! Tag cannot be empty.');
+  core.setFailed("ERROR! Tag cannot be empty.");
 }
 
 try {
   run();
-} catch(e) {
+} catch (e) {
   core.setFailed(e);
 }
