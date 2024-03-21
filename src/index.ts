f13bec51e7c5026a0aabcac9084f4734289ae4db
Dwi Siswanto <me@dw1.io>
2024-03-22 00:08:02 +0700

    feat: cache the bin
    Signed-off-by: Dwi Siswanto <me@dw1.io>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {
-  core, fs, https, io, os,
-  path, shell, TMP_DIR
+  cache, core, fs, https, io,
+  os, path, shell, TMP_DIR
 } from './const';
 
 const inputOverwrite = core.getBooleanInput('overwrite', {required: false});
@@ -46,7 +46,7 @@ async function get_slim() {
       VER = inputVersion;
     }
   } catch {
-    throw new Error('ERROR! Could not retrieve the current Slim version number.');
+    throw new Error(`ERROR! Could not get the Slim version ${VER}.`);
   }
   
   URL = `https://downloads.dockerslim.com/releases/${VER}`;
@@ -84,40 +84,65 @@ async function get_slim() {
   // Derive the filename
   FILENAME = `dist_${DIST}.${EXT}`;
 
-  const file = fs.createWriteStream(path.join(TMP_DIR, FILENAME));
-  await new Promise((resolve, reject) => {
-    https.get(`${URL}/${FILENAME}`, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
-        resolve(file);
+  // Get the bin from cache
+  const cachePrefix = `slim-${VER}`;
+  const cacheKey = `${cachePrefix}-${DIST}`;
+  const cachePath = path.join('/tmp', cacheKey);
+
+  try {
+    const cacheResult = await cache.restoreCache(
+      [ cachePath ], cacheKey, [ `${cachePrefix}-` ]
+    );
+
+    if (typeof cacheResult === 'undefined') {
+      throw new Error(`ERROR! Cache miss: ${cacheKey} was not found in the cache.`)
+    }
+
+    core.debug(`${cacheKey} cache was restored.`)
+
+    SLIM_PATH = cachePath;
+  } catch (e) {
+    const file = fs.createWriteStream(path.join(TMP_DIR, FILENAME));
+    await new Promise((resolve, reject) => {
+      https.get(`${URL}/${FILENAME}`, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+          file.close();
+          resolve(file);
+        });
+      }).on('error', (error) => {
+        fs.unlinkSync(path.join(TMP_DIR, FILENAME));
+        reject(error);
       });
-    }).on('error', (error) => {
-      fs.unlinkSync(path.join(TMP_DIR, FILENAME));
-      reject(error);
     });
-  });
 
-  core.debug(`Unpacking ${path.join(TMP_DIR, FILENAME)}`);
-  if (EXT === 'zip') {
-    const extract = require('extract-zip');
-    await extract(path.join(TMP_DIR, FILENAME), {
-      dir: TMP_DIR
-    });
-  } else if (EXT === 'tar.gz') {
-    const tar = require('tar');
-    await tar.x({
-      file: path.join(TMP_DIR, FILENAME),
-      cwd: TMP_DIR
-    });
-  } else {
-    throw new Error('ERROR! Unexpected file extension.');
+    core.debug(`Unpacking ${path.join(TMP_DIR, FILENAME)}`);
+    if (EXT === 'zip') {
+      const extract = require('extract-zip');
+      await extract(path.join(TMP_DIR, FILENAME), {
+        dir: TMP_DIR
+      });
+    } else if (EXT === 'tar.gz') {
+      const tar = require('tar');
+      await tar.x({
+        file: path.join(TMP_DIR, FILENAME),
+        cwd: TMP_DIR
+      });
+    } else {
+      throw new Error('ERROR! Unexpected file extension.');
+    }
+
+    SLIM_PATH = path.join(TMP_DIR, `dist_${DIST}`);
+
+    core.debug(`Copying ${SLIM_PATH} -> (${cachePath})`);
+    await io.cp(SLIM_PATH, cachePath, { recursive: true, force: true });
+
+    const cacheId = await cache.saveCache([ cachePath ], cacheKey);
+    core.debug(`${cacheKey} cache saved (ID: ${cacheId})`);
+  } finally {
+    core.addPath(SLIM_PATH);
+    core.info(`Using slim version ${VER}`);
   }
-
-  SLIM_PATH = path.join(TMP_DIR, `dist_${DIST}`);
-  core.addPath(SLIM_PATH);
-
-  core.info(`Using slim version ${VER}`);
 }
 
 async function run() {
@@ -143,7 +168,7 @@ async function run() {
     throw new Error('ERROR! Cannot build over target');
   }
 
-  const [image, tag] = report.target_reference.split(':')
+  const [image, tag] = report.target_reference.split(':');
 
   if (inputOverwrite && tag) {
     core.info(`Overwriting ${image}:${tag} with slimmed version`);
